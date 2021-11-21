3fe3126d4c3b9ebc521270cc13b4a9ebc0accbf8
delashu <53063128+delashu@users.noreply.github.com>
2021-11-21 11:09:56 -0500

    streamlit dashboard adds

---
+++ scripts/dashboard/icu_dash.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import statsmodels.api as sm
+#set wide format
+st.set_page_config(layout="wide")
