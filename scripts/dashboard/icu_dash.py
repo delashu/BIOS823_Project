486d29a7ce217ee4494bb9a64f530344b3ae50c9
delashu <53063128+delashu@users.noreply.github.com>
2021-11-21 11:36:30 -0500

    dash edits

--- scripts/dashboard/icu_dash.py
+++ scripts/dashboard/icu_dash.py
@@ -6,3 +6,6 @@ import plotly.graph_objects as go
 import statsmodels.api as sm
 #set wide format
 st.set_page_config(layout="wide")
+#read in training demo dat 
+#load in the training data
+df = pd.read_pickle('../modeling/demo_training.pkl')
