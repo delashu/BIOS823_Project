fa800512f2397a8b9585b974958ac32f58c527b3
Saahithi Rao <42307507+saahithirao@users.noreply.github.com>
2021-11-23 18:55:11 -0500

    Update icu_dash.py

--- scripts/dashboard/icu_dash.py
+++ scripts/dashboard/icu_dash.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import sklearn
+from sklearn.ensemble import GradientBoostingClassifier
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import statsmodels.api as sm
 import joblib
 import pickle
 import xgboost
